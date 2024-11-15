7da509723ce77534e149cf8b0ff3b7d775b16776
Taylor Cowley <tcowley@secturion.com>
2024-11-14 17:19:43 -0700

    day 7 part 1 complete

--- src/day7.zig
+++ src/day7.zig
@@ -34,17 +34,185 @@ const expect = std.testing.expect;
 const stdout = std.io.getStdOut().writer();
 const Chameleon = @import("chameleon.zig");
 
+const phand = struct { str: []const u8, bet: u32, strength: u8 };
+
+fn get_index_from_char(char: u8) u8 {
+    switch (char) {
+        'A' => {
+            return 0;
+        },
+        'K' => {
+            return 1;
+        },
+        'Q' => {
+            return 2;
+        },
+        'J' => {
+            return 3;
+        },
+        'T' => {
+            return 4;
+        },
+        '9' => {
+            return 5;
+        },
+        '8' => {
+            return 6;
+        },
+        '7' => {
+            return 7;
+        },
+        '6' => {
+            return 8;
+        },
+        '5' => {
+            return 9;
+        },
+        '4' => {
+            return 10;
+        },
+        '3' => {
+            return 11;
+        },
+        '2' => {
+            return 12;
+        },
+        else => {
+            return 0xff;
+        }, // TODO: maybe make a real error
+    }
+}
+
 fn get_strength(hand: []const u8) u8 {
     // TODO: not implemented yet
-    return 0;
+    if (hand.len != 5) {
+        std.debug.print("hand {s} length INCORRECT: {d}", .{ hand, hand.len });
+    }
+
+    // index, value
+    // 0 1 2 3 4 5 6 7 8 9 10 11 12
+    // A K Q J 0 9 8 7 6 5 4  3  2
+    var count_values = [_]u8{0} ** 13;
+
+    for (hand) |char| {
+        count_values[get_index_from_char(char)] += 1;
+    }
+
+    //std.debug.print("hand xxxxx values: . 0  1  2  3  4  5  6  7  8  9  10 11 12\n", .{});
+    std.debug.print("                     A  K  Q  J  0  9  8  7  6  5  4  3  2\n", .{});
+    std.debug.print("hand {s} values: {any}\n", .{ hand, count_values });
+
+    // if 5 exists in count_values, it's 5 of a kind
+    // if 4 exists in count_values, it's 4 of a kind
+    // if 3 exists in count_values AND 2 exists, it's full house
+    // if 3 exists in count_values, it's 3 of a kind
+    // if 2 2's exist in count_values, it's 2 pair
+    // if 2 exist in count_values, it's 1 pair
+    // Else it's nothing (high card)
+
+    // pub fn containsAtLeast(comptime T: type, haystack: []const T, expected_count: usize, needle: []const T) bool
+    if (std.mem.containsAtLeast(u8, &count_values, 1, &[_]u8{5})) {
+        std.debug.print("5 of a kind\n", .{});
+        return 7;
+    } else if (std.mem.containsAtLeast(u8, &count_values, 1, &[_]u8{4})) {
+        std.debug.print("4 of a kind\n", .{});
+        return 6;
+    } else if (std.mem.containsAtLeast(u8, &count_values, 1, &[_]u8{3}) and std.mem.containsAtLeast(u8, &count_values, 1, &[_]u8{2})) {
+        std.debug.print("full house\n", .{});
+        return 5;
+    } else if (std.mem.containsAtLeast(u8, &count_values, 1, &[_]u8{3})) {
+        std.debug.print("3 of a kind\n", .{});
+        return 4;
+    } else if (std.mem.containsAtLeast(u8, &count_values, 2, &[_]u8{2})) {
+        std.debug.print("2pair\n", .{});
+        return 3;
+    } else if (std.mem.containsAtLeast(u8, &count_values, 1, &[_]u8{2})) {
+        std.debug.print("pair\n", .{});
+        return 2;
+    } else if (std.mem.containsAtLeast(u8, &count_values, 1, &[_]u8{5})) {
+        std.debug.print("nothing\n", .{});
+        return 1;
+    } else { // TODO: error here
+        std.debug.print("ERROR\n", .{});
+        return 0;
+    }
 }
+
+fn phandGreaterThan(poop: void, lhs: phand, rhs: phand) bool {
+    _ = poop;
+    // The strengths differ
+    if (lhs.strength > rhs.strength) {
+        return true;
+    } else if (lhs.strength < rhs.strength) {
+        return false;
+    }
+
+    // The strengths are the same. Time to compare
+    for (0..5) |index| {
+        const lhs_char_index = get_index_from_char(lhs.str[index]);
+        const rhs_char_index = get_index_from_char(rhs.str[index]);
+        if (lhs_char_index < rhs_char_index) {
+            // A greater index means the hand is weaker
+            return true;
+        } else if (lhs_char_index > rhs_char_index) {
+            // A lesser index means the hand is stronger
+            return false;
+        }
+    }
+
+    // The hands are identical.
+    return false;
+}
+fn phandLessThan(poop: void, lhs: phand, rhs: phand) bool {
+    return !phandGreaterThan(poop, lhs, rhs);
+}
+
+const phand_list = struct {
+    items: *[]phand,
+    fn swap(self: *phand_list, a: usize, b: usize) void {
+        const ctx = self;
+        return std.mem.swap(phand, &ctx.items[a], &ctx.items[b]);
+        //return std.mem.swap(phand, ctx.items[a], ctx.items[b]);
+    }
+    fn lessThan(self: *phand_list, lhs_i: usize, rhs_i: usize) bool {
+        //pub fn lessThan(ctx: type, lhs_i: phand, rhs_i: phand) bool {
+        const ctx = self;
+        const lhs: phand = ctx.items[lhs_i];
+        const rhs: phand = ctx.items[rhs_i];
+        //_ = ctx;
+        //const lhs = lhs_i;
+        //const rhs = rhs_i;
+
+        // The strengths differ
+        if (lhs.strength < rhs.strength) {
+            return true;
+        } else if (lhs.strength > rhs.strength) {
+            return false;
+        }
+
+        // The strengths are the same. Time to compare
+        for (0..5) |index| {
+            const lhs_char_index = get_index_from_char(lhs.hand[index]);
+            const rhs_char_index = get_index_from_char(rhs.hand[index]);
+            if (lhs_char_index > rhs_char_index) {
+                // A greater index means the hand is weaker
+                return true;
+            } else if (lhs_char_index < rhs_char_index) {
+                // A lesser index means the hand is stronger
+                return false;
+            }
+        }
+
+        // The hands are identical.
+        return false;
+    }
+};
 
 // Strengths: 5 of kind(7), 4 of kind(6), full house(5), 3 of kind(4), 2pair(3), 1pair(2), nothing(1)
 // example: 32T3K 765
 // str: 32T3K
 // bet: 765
 // strength: pair (2)
-const phand = struct { str: []const u8, bet: u32, strength: u8 };
 fn get_hand(line: []const u8) !phand {
     //var hand = phand{.str=undefined, .bet=undefined};
     //hand.str = line[0..5];
@@ -134,8 +302,21 @@ pub fn main() !void {
         try list.append(hand);
     }
     for (list.items) |hand| {
-        try c.yellow().bold().printOut("hand: {s}, bet: {d}\n", .{ hand.str, hand.bet });
+        try c.yellow().bold().printOut("hand: {s}, bet: {d}, strength: {d}\n", .{ hand.str, hand.bet, hand.strength });
     }
+
+    // Sort the hands based on strength
+    try c.cyan().bold().printOut("\n SORTING NOW\n", .{});
+    std.mem.sort(phand, list.items, {}, phandLessThan);
+
+    // Print winnings
+    var total_winnings: u64 = 0;
+    for (list.items, 1..) |hand, index| {
+        const winnings = index * hand.bet;
+        total_winnings += winnings;
+        try c.green().bold().printOut("hand: {s}, bet: {d}, strength: {d}, index: {d}, winnings: {d}\n", .{ hand.str, hand.bet, hand.strength, index, winnings });
+    }
+    try c.green().bold().printOut("\ntotal winnings: {d}\n", .{total_winnings});
 
     //var races = std.ArrayList(race).init(allocator);
     //defer races.deinit();
